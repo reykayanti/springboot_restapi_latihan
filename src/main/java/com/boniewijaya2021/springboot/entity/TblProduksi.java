03479de77374382af15f72ae4fe43007de58ebf8
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2023-05-18 15:46:48 +0700

    Membuar table baru
    + ada Timestampnya

--- src/main/java/com/boniewijaya2021/springboot/entity/TblProduksi.java
+++ src/main/java/com/boniewijaya2021/springboot/entity/TblProduksi.java
@@ -1,24 +1,59 @@
 package com.boniewijaya2021.springboot.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tbl_produksi", schema = "sample")
 public class TblProduksi implements Serializable {
 
 //    private static final long serialversionID = 1L;
 
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product)id", nullable = false)
-    private Long id;
-    @Column(name = "product_name", nullable = false)
-    private String name;
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "uuid2")
+
+    @Column(name = "id_produksi", nullable = false)
+    private UUID id_produksi;
+
+    @Column(name = "nama_barang", nullable = false)
+    private String namaBarang;
+
+    @Column(name = "tipe_barang", nullable = false)
+    private String tipeBarang;
+
+    @Column(name = "asal_barang", nullable = false)
+    private String asalBarang;
+
+    @Column(name = "biaya_produksi", nullable = false)
+    private Integer biayaProduksi;
+
+//    @Column(name = "tanggal_produksi", nullable = false)
+//    private Timestamp tanggalProduksi;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal (TemporalType.TIMESTAMP) //untuk mengubah data ke waktu dan tanggal
+    @CreatedDate
+    private Date tanggalProduksi;
+
+//    last modif or updated
+//    @Column(nullable = false)
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @LastModifiedDate
 
 //    public Produksi(Long id, String name){
 //        this.id = id;
